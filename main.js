7def3246efc86746bb912d278e92ead5ad31dd34
dizehacioglu <dh1272@nyu.edu>
2015-05-28 20:54:22 -0600

    Tried directions

--- main.js
+++ main.js
@@ -1,34 +1,73 @@
+// var directionsDisplay;
+// var map;
+
+// function initialize() {
+// 	directionsDisplay = new google.maps.DirectionsRenderer();
+//     var mapOptions = {
+//       center: { lat: -34.397, lng: 150.644},
+//       zoom: 8
+//     };
+//     var map = new google.maps.Map(document.getElementById('map-canvas'),
+//         mapOptions);
+//     directionsDisplay.setMap(map);
+//   }
+
+
+
+// // google.maps.event.addDomListener(window, 'load', initialize);
+
+// var directionsService = new google.maps.DirectionsService();
+
+// var calcRoute = function(){
+// 	var start = 'chicago, il';
+// 	var end = 'joplin, mo';
+// 	var request = {
+// 		origin: start,
+// 		destination: end,
+// 		travelMode: google.maps.TravelMode.DRIVING
+// 	};
+// 	directionsService.route(request, function(result, status){
+// 		if(status == google.maps.DirectionsStatus.OK){
+// 			directionsDisplay.setDirections(result);
+// 		}
+// 		else{
+// 			console.log('Error');
+// 		}
+// 	});
+// };
+
+// calcRoute();
+
+
+
+// // console.log('map');
+
 var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 var map;
 
 function initialize() {
-	directionsDisplay = new google.maps.DirectionsRenderer();
-    var mapOptions = {
-      center: { lat: -34.397, lng: 150.644},
-      zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-    directionsDisplay.setMap(map);
-  }
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom:7,
+    center: chicago
+  };
+  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  directionsDisplay.setMap(map);
+}
 
-
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-var directionsService = new google.maps.DirectionsService();
-
-var calcRoute = function(){
-	var start = 'chicago, il';
-	var end = 'joplin, mo';
-	var request = {
-		origin: start,
-		destination: end,
-		travelMode: google.maps.TravelMode.DRIVING
-	};
-};
-
-
-
-
-// console.log('map');
+function calcRoute() {
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+  var request = {
+    origin:start,
+    destination:end,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+    }
+  });
+}
