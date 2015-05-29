388372feb5354c1d75efa0fd565486e926d1a6a4
dizehacioglu <dh1272@nyu.edu>
2015-05-29 00:16:41 -0600

    Added form

--- main.js
+++ main.js
@@ -1,73 +1,86 @@
-// var directionsDisplay;
-// var map;
+var directionsDisplay;
+var map;
 
-// function initialize() {
-// 	directionsDisplay = new google.maps.DirectionsRenderer();
-//     var mapOptions = {
-//       center: { lat: -34.397, lng: 150.644},
-//       zoom: 8
-//     };
-//     var map = new google.maps.Map(document.getElementById('map-canvas'),
-//         mapOptions);
-//     directionsDisplay.setMap(map);
-//   }
+function initialize() {
+	directionsDisplay = new google.maps.DirectionsRenderer();
+    var mapOptions = {
+      center: { lat: -34.397, lng: 150.644},
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+    directionsDisplay.setMap(map);
+  }
 
+google.maps.event.addDomListener(window, 'load', initialize);
 
+var directionsService = new google.maps.DirectionsService();
 
-// // google.maps.event.addDomListener(window, 'load', initialize);
+var calcRoute = function(){
+  
+  var start;
+  var end;
 
-// var directionsService = new google.maps.DirectionsService();
+  $('#route').on('submit', function(e){
+    start = $(this).find('[name=origin]').val();
+    end = $(this).find('[name=destination]').val();
 
-// var calcRoute = function(){
-// 	var start = 'chicago, il';
-// 	var end = 'joplin, mo';
-// 	var request = {
-// 		origin: start,
-// 		destination: end,
-// 		travelMode: google.maps.TravelMode.DRIVING
-// 	};
-// 	directionsService.route(request, function(result, status){
-// 		if(status == google.maps.DirectionsStatus.OK){
-// 			directionsDisplay.setDirections(result);
-// 		}
-// 		else{
-// 			console.log('Error');
-// 		}
-// 	});
-// };
+    console.log(start, end);
+  }
 
-// calcRoute();
+  
 
+	
+	var request = {
+		origin: start,
+		destination: end,
+		travelMode: google.maps.TravelMode.DRIVING
+	};
+	directionsService.route(request, function(result, status){
+		if(status == google.maps.DirectionsStatus.OK){
+			directionsDisplay.setDirections(result);
+		}
+		else{
+			console.log('Error');
+		}
+	});
+};
+
+calcRoute();
+
+console.log()
 
 
 // // console.log('map');
 
-var directionsDisplay;
-var directionsService = new google.maps.DirectionsService();
-var map;
+// var directionsDisplay;
+// var directionsService = new google.maps.DirectionsService();
+// var map;
 
-function initialize() {
-  directionsDisplay = new google.maps.DirectionsRenderer();
-  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
-  var mapOptions = {
-    zoom:7,
-    center: chicago
-  };
-  map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  directionsDisplay.setMap(map);
-}
+// function initialize() {
+//   directionsDisplay = new google.maps.DirectionsRenderer();
+//   var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+//   var mapOptions = {
+//     zoom:7,
+//     center: chicago
+//   };
+//   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+//   directionsDisplay.setMap(map);
+// }
 
-function calcRoute() {
-  var start = document.getElementById("start").value;
-  var end = document.getElementById("end").value;
-  var request = {
-    origin:start,
-    destination:end,
-    travelMode: google.maps.TravelMode.DRIVING
-  };
-  directionsService.route(request, function(result, status) {
-    if (status == google.maps.DirectionsStatus.OK) {
-      directionsDisplay.setDirections(result);
-    }
-  });
-}
+// function calcRoute() {
+//   var start = document.getElementById("start").value;
+//   var end = document.getElementById("end").value;
+//   var request = {
+//     origin:start,
+//     destination:end,
+//     travelMode: google.maps.TravelMode.DRIVING
+//   };
+//   directionsService.route(request, function(result, status) {
+//     if (status == google.maps.DirectionsStatus.OK) {
+//       directionsDisplay.setDirections(result);
+//     }
+//   });
+// }
+
+// google.maps.event.addDomListener(window, 'load', initialize);
