69601781466bc1892d622af8b8ca29280baca9d1
dizehacioglu <dh1272@nyu.edu>
2015-05-29 17:05:47 -0600

    Added direction servcies

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 var map;
 
 function initialize() {
 	directionsDisplay = new google.maps.DirectionsRenderer();
     var mapOptions = {
-      center: { lat: -34.397, lng: 150.644},
+      center: { lat: 40.015, lng: -105.27},
       zoom: 8
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
+
     directionsDisplay.setMap(map);
-  }
+ }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
 
-var directionsService = new google.maps.DirectionsService();
+initialize();
+
+
 
 var calcRoute = function(){
   
-  var start;
-  var end;
+	var start;
+	var end;
 
-  $('#route').on('submit', function(e){
-    start = $(this).find('[name=origin]').val();
-    end = $(this).find('[name=destination]').val();
+	$('#route').on('submit', function(e){
+		e.preventDefault();
 
-    console.log(start, end);
-  }
+		start = $(this).find('[name=origin]').val();
+		end = $(this).find('[name=destination]').val();
 
-  
-
-	
-	var request = {
-		origin: start,
-		destination: end,
-		travelMode: google.maps.TravelMode.DRIVING
-	};
-	directionsService.route(request, function(result, status){
-		if(status == google.maps.DirectionsStatus.OK){
-			directionsDisplay.setDirections(result);
-		}
-		else{
-			console.log('Error');
-		}
+		var request = {
+			origin: start,
+			destination: end,
+			travelMode: google.maps.TravelMode.DRIVING
+		};
+		
+		directionsService.route(request, function(result, status){
+			if(status == google.maps.DirectionsStatus.OK){
+				directionsDisplay.setDirections(result);
+			}
+			else{
+				console.log('Error');
+			}
+		});
 	});
 };
 
 calcRoute();
-
-console.log()
 
 
 // // console.log('map');
